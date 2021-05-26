8611dce9b7abb208322dbce4ce7f217b7fc37ed3
Fabco <contact@fabco.tech>
2021-05-26 13:54:57 +0200

    get-started -> developer + redesign

--- nav/Nav.js
+++ nav/Nav.js
@@ -65,7 +65,7 @@ export class Nav extends React.Component {
                 <div className={`dropdown`}>
                   <a href="/community.html">Community Overview</a>
                   <a href="/research.html">Research</a>
-                  <a href="/get-started.html">Get started</a>
+                  <a href="/developer.html">Developer</a>
                   <a href="/team.html">Team</a>
                   <a href="https://blog.rchain.coop/" target="_blank">
                     Blog
@@ -107,7 +107,7 @@ export class Nav extends React.Component {
             <a href="/community.html">Community / Join</a>
             <a href="/research.html">Research</a>
             <a href="/whitepaper.html">Whitepaper</a>
-            <a href="/get-started.html">Get started</a>
+            <a href="/developer.html">Developer</a>
             <a href="/team.html">Team</a>
             <a href="https://developer.rchain.coop/" target="_blank">
               Developers
